f332edc558a33871b68d3388937951e313e49c43
Jubayer-Cmd <sheikhabujubayer@gmail.com>
2025-03-01 16:56:28 +0600

    fixed button responsive

--- src/config/button-config.ts
+++ src/config/button-config.ts
@@ -5,10 +5,10 @@ export const buttonConfig = {
         backgroundColor: "#A953FF",
         top: "75%",
         left: "50%",
-        width: "70%",
-        height: "48px",
-        borderRadius: "8px",
-        fontSize: "24px",
+        // width: "70%",
+        // height: "48px",
+        // borderRadius: "8px",
+        // fontSize: "24px",
     },
 };
 
